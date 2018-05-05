83d9eb7062c7f2a488e21d6038231da4a0f520f0
alzepeda <31897748+alzepeda@users.noreply.github.com>
2018-05-05 11:15:32 -0600

    Add files via upload
    Database support added

--- app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeActivity.java
@@ -1,19 +1,60 @@
 package edu.utep.cs.cs4330.ifridge;
 
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 
 /**
- * Created by AnaZepeda on 4/17/18.
+ * Created by AnaZepeda on 5/3/18.
  */
 
-public class FridgeActivity extends AppCompatActivity{
+public class FridgeActivity extends AppCompatActivity {
+    FridgeDatabaseHelper fridgeDB;
+    EditText editIngredient;
+    Button addIngredient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fridge);
-        Intent intent = getIntent();
+        fridgeDB = new FridgeDatabaseHelper(this);
+
+        editIngredient = findViewById(R.id.editIngredient);
+        addIngredient = findViewById(R.id.addIngredientButton);
+    }
+
+    public void addButtonClicked(View view) {
+        boolean isInserted = fridgeDB.insertData(editIngredient.getText().toString());
+        if(isInserted = true)
+            Toast.makeText(FridgeActivity.this, "Ingredient Inserted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(FridgeActivity.this, "Ingredient not Inserted", Toast.LENGTH_SHORT).show();
+
+        Cursor res = fridgeDB.getAllData();
+        if(res.getCount() == 0){
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        ArrayList foods = new ArrayList<String>();
+        while(res.moveToNext()){
+            buffer.append(res.getString(1));
+            foods.add(buffer.toString());
+        }
+
+        ListAdapter listAdapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        RecyclerView listView = (RecyclerView)findViewById(R.id.recyclerViewforIngredients);
+        listView.setAdapter((RecyclerView.Adapter) listAdapter);
     }
 }
--- app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeDatabaseHelper.java
+++ app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeDatabaseHelper.java
@@ -1,21 +1,50 @@
 package edu.utep.cs.cs4330.ifridge;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * Created by AnaZepeda on 5/3/18.
+ */
+
 public class FridgeDatabaseHelper extends SQLiteOpenHelper {
-    public FridgeDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DATABASE_NAME = "Fridge.db";
+    public static final String TABLE_NAME = "fridge_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+    public FridgeDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 }
