e94c57b0545a407fec1dae9ef5d4700b164771b1
alzepeda <31897748+alzepeda@users.noreply.github.com>
2018-05-05 13:51:58 -0600

    View reflects database

--- app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/ifridge/FridgeActivity.java
@@ -3,16 +3,15 @@ package edu.utep.cs.cs4330.ifridge;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.Toast;
+
+import java.lang.String;
 
 import java.util.ArrayList;
 
@@ -33,28 +32,50 @@ public class FridgeActivity extends AppCompatActivity {
 
         editIngredient = findViewById(R.id.editIngredient);
         addIngredient = findViewById(R.id.addIngredientButton);
+
+        showIngredients();
+
     }
 
     public void addButtonClicked(View view) {
-        boolean isInserted = fridgeDB.insertData(editIngredient.getText().toString());
+        String ingredient = editIngredient.getText().toString();
+        Cursor res = fridgeDB.getAllData();
+        StringBuffer buffer;
+
+        while(res.moveToNext()){
+            buffer = new StringBuffer();
+            buffer.append(res.getString(1));
+            if(ingredient.equalsIgnoreCase(buffer.toString())){
+                Toast.makeText(FridgeActivity.this, "Item already contained", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        boolean isInserted = fridgeDB.insertData(ingredient);
         if(isInserted = true)
             Toast.makeText(FridgeActivity.this, "Ingredient Inserted", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(FridgeActivity.this, "Ingredient not Inserted", Toast.LENGTH_SHORT).show();
 
-        Cursor res = fridgeDB.getAllData();
-        if(res.getCount() == 0){
-            return;
-        }
-        StringBuffer buffer = new StringBuffer();
-        ArrayList foods = new ArrayList<String>();
-        while(res.moveToNext()){
-            buffer.append(res.getString(1));
-            foods.add(buffer.toString());
-        }
+        showIngredients();
+    }
 
-        ListAdapter listAdapter =  new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
-        RecyclerView listView = (RecyclerView)findViewById(R.id.recyclerViewforIngredients);
-        listView.setAdapter((RecyclerView.Adapter) listAdapter);
+    public void showIngredients() {
+        StringBuffer buffer;
+        ArrayList foods = new ArrayList<String>();
+
+        Cursor res = fridgeDB.getAllData();
+        if(res.getCount() != 0) {
+
+            while (res.moveToNext()) {
+                buffer = new StringBuffer();
+                buffer.append(res.getString(1));
+                foods.add(buffer.toString());
+            }
+
+            ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+            ListView listView = (ListView) findViewById(R.id.listViewforIngredients);
+            listView.setAdapter(listAdapter);
+        }
     }
 }
