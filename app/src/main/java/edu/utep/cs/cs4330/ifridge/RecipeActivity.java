3b067b58f42d55094e9f9b967b28bb72d7795970
Sebastian Gonzalez <33067271+seb1gonzalez@users.noreply.github.com>
2018-05-08 09:28:38 -0600

    RecipeAcivity started

--- app/src/main/java/edu/utep/cs/cs4330/ifridge/RecipeActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/ifridge/RecipeActivity.java
@@ -9,20 +9,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by AnaZepeda on 4/17/18.
- */
-
 public class RecipeActivity extends AppCompatActivity{
     RecipeDatabaseHelper recipeDatabaseHelper;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe);
         recipeDatabaseHelper =new RecipeDatabaseHelper(this);
-
-
 
     }
 }
